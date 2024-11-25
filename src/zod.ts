23ded21021b0692559c19ca39ec27bb3e721d5b6
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-25 09:30:49 -0300

    Zod: Mensagens de erro

--- src/zod.ts
+++ src/zod.ts
@@ -1,11 +1,11 @@
 import z from "zod";
 
 const pattern = z.object({
-    name: z.string()
+    age: z.number({ required_error: "Idade é obrigatoria", invalid_type_error: "Idade precisa ser um número!" }).gte(18, "Precisa ser maior de idade!")
 });
 
 const result = pattern.parse({
-    name: "João",
+    age: 15
 })
 console.log(result);
 
