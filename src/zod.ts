70fe9b96bc53293702021b06fa6aca1508a13bb2
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-25 08:10:46 -0300

    Entendendo o básico do zod

--- src/zod.ts
+++ src/zod.ts
@@ -1,1 +1,14 @@
 import z from "zod";
+
+
+const schema = z.object({
+    name: z.string().min(2),
+    email: z.string().email(),
+    age: z.number().min(18).max(120)
+})
+
+let data = {
+    name: "João",
+    email: "joao@gmail.com",
+    age: 19
+};
