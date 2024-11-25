441bc21ce418b6c0df17d10abc1d4bbf75139e44
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-25 08:49:12 -0300

    Zod: Opcionais

--- src/zod.ts
+++ src/zod.ts
@@ -1,31 +1,16 @@
 import z from "zod";
 
-
-const schema = z.object({
-    name: z.string().min(2),
-    email: z.string().email(),
-    age: z.number().min(18).max(120),
-    status: z.boolean(),
-    characteristics: z.array(
-        z.object({
-        name: z.string(),
-        value: z.number(),
-    }))
+const pattern = z.object({
+    name: z.string(),
+    age: z.number(),
+    active: z.boolean(),
+    birthDate: z.date(),
+    algo: z.null(),
+    nunca: z.never()
 });
 
-type User = z.infer<typeof schema>;
-
-let data: User = {
-    name: "João",
-    email: "joao@gmail.com",
-    age: 18,
-    status: true,
-    characteristics: [
-        {name: "Mana", value: 10},
-        {name: "Escudo", value: 50}
-    ]
-};
-
-const result = schema.parse(data);
+const result = pattern.parse({
+    name: "João"
+})
 console.log(result);
 
