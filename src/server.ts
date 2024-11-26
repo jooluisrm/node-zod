05818598417b7c46c0dd6b25ee65868cddf18aa0
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-26 10:08:00 -0300

    Node: Caso de uso: Validando request de API

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,36 @@
 import express from "express";
+import z from "zod";
 
 const server = express();
 
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
 server.get("/ping", (req, res) => {
-    res.json({pong: true});
+    res.json({ pong: true });
+});
+
+server.post("/user", (req, res) => {
+    const userSchema = z.object({
+        name: z.string().min(2),
+        email: z.string().email(),
+        age: z.number().min(18).max(120)
+    })
+
+    const result = userSchema.safeParse(req.body);
+
+    if (!result.success) {
+        res.json({ error: "dados inválidos!" });
+        return;
+    }
+
+    const { name, email, age } = result.data;
+    // processo
+
+    console.log("Processando...");
+    console.log(name, email, age);
+
+    res.status(201).json({ result: 'tudo ok' });
 });
 
 server.listen(3000, () => {
